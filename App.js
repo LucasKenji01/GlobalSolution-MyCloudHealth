bc880a5153bceb19ad224aa2912f2dc96c8fbb7a
Lucas Kenji <rm94233@fiap.com.br>
2023-11-15 11:00:32 -0300

    Estrutura do projeto

---
+++ App.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+import LoginScreen from "./src/screens/LoginScreen";
+import CadastroScreen from "./src/screens/CadastroScreen";
+import HomeScreen from "./src/screens/HomeScreen";
+import HistoricoScreen from "./src/screens/HistoricoScreen";
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{
+            title: '',
+            headerTransparent: true,
+            headerShown: false,
+          }} />
+        <Stack.Screen
+          name="Cadastro"
+          component={CadastroScreen}
+          options={{
+            title: '',
+            headerTransparent: true,
+            headerShown: false,
+          }} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreenScreen}
+          options={{
+            title: '',
+            headerTransparent: true,
+            headerShown: false,
+          }} />
+        <Stack.Screen
+          name="Historico"
+          component={HistoricoScreen}
+          options={{
+            title: '',
+            headerTransparent: true,
+            headerShown: false,
+          }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
---
+++ src/screens/HistoricoScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function HistoricoScreen() {
+  return (
+    <View>
+      <Text>HistoricoScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
